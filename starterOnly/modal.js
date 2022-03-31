abb4f4da76f6e6526624725a79cd8af686e05cac
Spacelnvader <la-douce-64@hotmail.fr>
2022-03-31 12:54:11 +0200

    vérification des champs du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,3 +27,36 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Below Function Executes On Form Submit
+function validate() {
+  // Storing Field Values In Variables
+  const firstName = document.getElementById("first").value;
+  const lastName = document.getElementById("last").value;
+  const email = document.getElementById("email").value;
+  const birthDate = document.getElementById("birthDate").value;
+  const numberOfTournament = document.getElementById("quantity").value;
+  const radioButtonsLocation = document.querySelectorAll('input[name="location"]');
+  
+
+  // Regular Expression For Email
+  var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+
+  // Conditions
+  if (firstName != '' && lastName != '' && email != '' && birthDate != '' && numberOfTournament != '') {
+  if (email.match(emailReg)) {
+  if(radioButtonsLocation.checked) {
+  alert("Inscription enregistrée !");
+  return true;
+  } else {
+      alert("Choisissez un lieu pour le tournoi de cette année ");
+      return false;
+  }
+  } else {
+      alert("Adresse e-mail invalide");
+      return false;
+  }
+  } else {
+      alert("Tous les champs doivent être complété");
+      return false;
+  }
+  }
